07fcb71753894fa835d07a98e2995d4b5f41b9cd
Jeremy Postlethwaite <jeremy.postlethwaite@gmail.com>
2023-11-21 23:22:32 -0800

    PG-5 - added CanResetPassword and MustVerifyEmail to user

--- src/Models/AbstractUser.php
+++ src/Models/AbstractUser.php
@@ -7,8 +7,11 @@
 namespace GammaMatrix\Playground\Test\Models;
 
 use Illuminate\Auth\Authenticatable;
+use Illuminate\Auth\MustVerifyEmail;
+use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -18,10 +21,15 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
  *
  * @see Illuminate\Foundation\Auth\User
  */
-abstract class AbstractUser extends Model implements AuthenticatableContract, AuthorizableContract
+abstract class AbstractUser extends Model implements
+    AuthenticatableContract,
+    AuthorizableContract,
+    CanResetPasswordContract
 {
     use Authenticatable;
     use Authorizable;
+    use CanResetPassword;
+    use MustVerifyEmail;
     use HasFactory;
 
     protected $fillable = ['email'];
