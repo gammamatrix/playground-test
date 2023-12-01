416536820856a7e562db8cd3ef4da0d344ed41d8
Jeremy Postlethwaite <jeremy.postlethwaite@gmail.com>
2023-11-30 20:41:45 -0800

    PG-29

---
+++ src/Feature/Http/Controllers/Resource/IndexTrait.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * GammaMatrix
+ */
+
+namespace GammaMatrix\Playground\Test\Feature\Http\Controllers\Resource;
+
+use GammaMatrix\Playground\Test\Models\User;
+use GammaMatrix\Playground\Test\Models\UserWithSanctum;
+use Tests\Feature\GammaMatrix\Playground\Matrix\Resource\TestCase;
+
+/**
+ * \GammaMatrix\Playground\Test\Feature\Http\Controllers\Resource\IndexTrait
+ *
+ */
+trait IndexTrait
+{
+    public function test_guest_cannot_render_index_view()
+    {
+        $fqdn = $this->fqdn;
+
+        $model = $fqdn::factory()->create();
+
+        $url = route(sprintf(
+            '%1$s.index',
+            $this->packageInfo['model_route']
+        ), [
+            $this->packageInfo['model_slug'] => $model->id,
+        ]);
+
+        $response = $this->get($url);
+        $response->assertRedirect(route('login'));
+    }
+
+    public function test_index_view_rendered_by_user()
+    {
+        $fqdn = $this->fqdn;
+
+        $model = $fqdn::factory()->create();
+
+        $user = UserWithSanctum::factory()->create();
+
+        $index = route($this->packageInfo['model_route']);
+
+        $url = route(sprintf(
+            '%1$s.index',
+            $this->packageInfo['model_route']
+        ), [
+            $this->packageInfo['model_slug'] => $model->id,
+        ]);
+
+        $response = $this->actingAs($user)->get($url);
+
+        $response->assertStatus(200);
+
+        $this->assertAuthenticated();
+    }
+
+    public function test_index_info_with_user_using_json()
+    {
+        $fqdn = $this->fqdn;
+
+        $model = $fqdn::factory()->create();
+
+        $user = UserWithSanctum::factory()->create();
+
+        $url = route(sprintf(
+            '%1$s.index',
+            $this->packageInfo['model_route']
+        ), [
+            $this->packageInfo['model_slug'] => $model->id,
+        ]);
+
+        $response = $this->actingAs($user)->getJson($url);
+
+        $response->assertStatus(200);
+        // $response->dump();
+
+        $response->assertJsonStructure([
+            'data',
+            'meta',
+        ]);
+        $this->assertAuthenticated();
+    }
+}
